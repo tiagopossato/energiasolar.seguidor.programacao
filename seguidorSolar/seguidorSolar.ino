c89795415ccd1cc67659b5758b503ff4f69b5a0f
nectar <nectar@Nectar-PC>
2017-04-25 22:15:51 -0300

    novo protocolo de comunicacao serial

--- seguidorSolar/seguidorSolar.ino
+++ seguidorSolar/seguidorSolar.ino
@@ -5,7 +5,9 @@
       posição do eixo onde existir a menor diferença entre os sensores
       e ao mesmo tempo o maior índice de incidência de luz.
 */
+
 #define DEBUG
+#include "util.h"
 #include "SeguidorSolar.h"
 #define LDR1 A2
 #define LDR2 A3
@@ -30,11 +32,10 @@ Eixo eixoDiario;
 /**********CONTROLADOR DE PLACA SOLAR**********/
 SeguidorSolar seguidor;
 
-String inputString = "";         // a string to hold incoming data
-
+char input[64];
 
 void setup () {
-  inputString.reserve(3);
+  input[0] = '\0';
   //inicia
   Serial.begin(9600);
   /**********INSERE OS PARÂMETROS DOS OBEJTOS**********/
@@ -42,8 +43,8 @@ void setup () {
   eixoDiario.motor.esquerda = IN2;
   eixoDiario.motor.habilita = ENA;
   eixoDiario.pot.pino = POT1;
-  eixoDiario.pot.minimo = 100;
-  eixoDiario.pot.maximo = 687;
+  eixoDiario.pot.minimo = 110;
+  eixoDiario.pot.maximo = 697;
   eixoDiario.sensores.fdc1 = FDC1;
   eixoDiario.sensores.fdc2 = FDC2;
   eixoDiario.sensores.ldr1 = LDR1;
@@ -57,8 +58,6 @@ void setup () {
 
 
 void loop () {
-  static uint8_t pos;
-  static uint8_t lastPos;
   static unsigned long previousMillis = 0;
   static unsigned long previousMillis2 = 0;
   unsigned long currentMillis = millis();
@@ -66,27 +65,16 @@ void loop () {
   if (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
-    // add it to the inputString:
-
-    Serial.print(inChar);
-    if (inChar != '\n')inputString += inChar;
-
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '\n') {
-      if (inputString.length() >= 1) {
-        pos = inputString.toInt();
-        if (abs(lastPos - pos) >= 5) {
-          seguidor.moveParaPosicao(&eixoDiario, pos);
-          eixoDiario.posicao.minima = eixoDiario.posicao.atual - 5;
-          eixoDiario.posicao.maxima = eixoDiario.posicao.atual + 5;
-          lastPos = pos;
-        }
+    if (inChar != '\n') {
+      sprintf(input, "%s%c", input, inChar);
+    }
+    if (inChar == '\n' || strlen(input) == 63) {
+      if (strlen(input) >= 1) {
+        trataComando(input);
       }
-      inputString = "";
+      input[0] = '\0';
     }
   }
-
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
@@ -105,3 +93,50 @@ void loop () {
   }
 #endif
 }
+
+/**
+   Trata os comandos recebidos via porta serial
+   LISTA DE COMANDOS
+   1/[posicao] -> Mova o painel para a posicao (em porcentagem). Ex.: 1:50
+   2/[dia]/[segundos] -> informa para a placa:
+                                            O dia atual, a de 1 a 365, a partir de 1º de janeiro.
+                                            O segundo atual a partir da meia-noite
+*/
+void trataComando(char *comando) {
+  uint16_t dia;
+  uint32_t segundo;
+  int8_t posicao;
+  static int8_t lastPos;
+
+  switch (extraiCodigo(comando)) {
+    case 1:
+      posicao = extraiCodigo(comando);
+      if (abs(lastPos - posicao) >= 3) {
+        lastPos = posicao;
+        seguidor.moveParaPosicao(&eixoDiario, posicao);
+        eixoDiario.posicao.minima = eixoDiario.posicao.atual - 5;
+        eixoDiario.posicao.maxima = eixoDiario.posicao.atual + 5;
+      }
+      break;
+    case 2:
+      dia = extraiCodigo(comando);
+      segundo = extraiCodigo(comando);
+      posicao = calculaPosicao(dia, segundo);
+      if (posicao >= 0) {
+        if (abs(lastPos - posicao) >= 3) {
+          lastPos = posicao;
+          seguidor.moveParaPosicao(&eixoDiario, posicao);
+          eixoDiario.posicao.minima = eixoDiario.posicao.atual - 5;
+          eixoDiario.posicao.maxima = eixoDiario.posicao.atual + 5;
+        }
+      }
+      break;
+    default:
+      break;
+  }
+
+
+
+}
+
+
---
+++ seguidorSolar/util.h
@@ -0,0 +1,75 @@
+//Função para resetar o programa
+void(* resetFunc) (void) = 0;
+
+
+uint32_t extraiCodigo(char *entrada) {
+  char i = 0;
+  String tmp;
+  tmp.reserve(10);
+  for (i = 0;; i++) {
+    //aceita valores até 0 to 4,294,967,295
+    if (i > 10) break;
+    if (entrada[i] == '/') break;
+    tmp += entrada[i];
+  }
+
+  sprintf(entrada, entrada + i + 1);
+  Serial.print("Numero: ");
+  Serial.println(tmp.toInt());
+  return tmp.toInt();
+}
+
+
+
+/**
+   Calcula o segundo em que o sol nasce no dia
+*/
+uint32_t nascerDoSol(uint16_t dia) {
+  return (0.0000000302070774172166 * pow(dia, 5))
+         - (0.0000198076304492236 * pow(dia, 4))
+         + (0.0035950755 * pow(dia, 3))
+         - (0.2694919508 * pow(dia, 2))
+         + (48.27020716989 * dia)
+         + 19901.4743825752;
+}
+
+/**
+   Calcula o segundo em que o sol se põe no dia
+*/
+uint32_t porDoSol(uint16_t dia) {
+  return (0.0000000415881756309789 * pow(dia, 5))
+         - (0.000042747 * pow(dia, 4))
+         + (0.0155753852 * pow(dia, 3))
+         - (2.1677644896 * pow(dia, 2))
+         + (58.67243756 * dia)
+         + 69346.2344757363;
+}
+
+
+/**
+   Calcula posicao do painel pela hora do dia
+*/
+int8_t calculaPosicao(uint16_t dia, uint32_t segundo) {
+  uint32_t nascerSol = nascerDoSol(dia);
+  uint32_t porSol = porDoSol(dia);
+  Serial.print("Nascer do sol: ");
+  Serial.println(nascerSol);
+  Serial.print("Por do sol: ");
+  Serial.println(porSol);
+  if (segundo < nascerSol || segundo > porSol) {
+    return -1;
+  }
+
+  uint32_t segundosPorDia =  porSol - nascerSol;
+  uint32_t segundosRelativos = (segundo - nascerSol) * 100 / segundosPorDia;
+  Serial.print(segundosRelativos);
+  Serial.println("% do dia.");
+
+  if (segundosRelativos >= 0 && segundosRelativos <= 100) {
+    Serial.print(segundosRelativos);
+    Serial.println("% do dia.");
+    return segundosRelativos;
+  }
+}
+
+
